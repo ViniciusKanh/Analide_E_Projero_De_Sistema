47e70263c4171e4051a0f8be967b9abf1e2bca96
Vinicius Santos <vinnyciussouza@outlook.com>
2022-04-21 11:42:06 -0300

    Criando O Projeto
    Desenvolvendo as Classes e a Conexão com o banco de Dados

---
+++ Projeto_BusSpot/Projeto/BancoDados/ConexaoBD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDados
+{
+    internal class ConexaoBD
+    {
+
+        public static string cn = "Data Sourse = VINICIUS-SANTOS; Initial Catalog = BusSpot; Integrated security = true ";
+    }
+}
